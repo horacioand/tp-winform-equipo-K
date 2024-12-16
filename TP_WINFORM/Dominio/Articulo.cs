3635e66666b16da273a0a6a8dad2b0c41fa9a86d
Horacio <horacioanadon00@gmail.com>
2024-12-16 17:33:34 -0300

    creacion clase articulo y acceso a datos

---
+++ TP_WINFORM/Dominio/Articulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    /*
+      Código de artículo.
+    Nombre.
+    Descripción.
+    Marca (seleccionable de una lista desplegable).
+    Categoría (seleccionable de una lista desplegable.
+    Imagen.
+    Precio.
+    */
+    public class Articulo
+    {
+        public int CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Categoría { get; set; }
+        public string Imagen { get; set; }
+        public float Precio { get; set; }
+    }
+}
