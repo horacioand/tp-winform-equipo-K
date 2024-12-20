d37a75e76478133d18b21787504ef201e5b7282f
Horacio <horacioanadon00@gmail.com>
2024-12-19 22:05:47 -0300

    desarrollo clase acceso a datos

--- TP_WINFORM/Negocio/AccesoDatos.cs
+++ TP_WINFORM/Negocio/AccesoDatos.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SqlClient;
+using Dominio;
 namespace Negocio
 {
     public class AccesoDatos
     {
+        SqlConnection conexion;
+        SqlCommand comando;
+        SqlDataReader reader;
+        public SqlDataReader Reader { get { return reader; } }
+
+
+        public AccesoDatos()
+        {
+            //atentos al nombre del servidor > vean si usan SQLEXPRESS01 o SQLEXPRESS
+            conexion = new SqlConnection("server=.\\SQLEXPRESS01; database=CATALOGO_P3_DB; integrated security=TRUE");
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        
+        public void cerrarConexion()
+        {
+            if (reader != null)
+            {
+                reader.Close();
+            }
+            conexion.Close();
+        }
     }
 }
